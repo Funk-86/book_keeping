779c28dca486b374cdc96b78c8e466c20b61badf
Funk-86 <2376569037@qq.com>
2026-09-23 15:37:54 +0800

    项目建立

---
+++ src/test/java/org/example/book_keeping/BookKeepingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.book_keeping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookKeepingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
